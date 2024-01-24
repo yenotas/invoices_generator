79d8ed29fa9eff22e60ed6173d92951ce74cbceb
Andrey Press <yenotas@gmail.com>
2024-01-24 19:24:22 +0100

    в JSON добавлен сдвиг по Y для подстановок после таблицы

--- modules/svg_templates_generator.py
+++ modules/svg_templates_generator.py
@@ -124,10 +124,6 @@ def generate_svg_templates(json_data, base_svg_file):
         stamp_line = soup.select("line[class*='stamp_line']")[0]
         magnet_stamp_y = float(stamp_line['y1'])/100  # позиция У для прицеливания штампа
 
-        top_line = float(horizontal_lines[1]['y1'])
-        bottom_line = float(horizontal_lines[2]['y1'])
-        items_line_height = bottom_line - top_line
-
         # Параметры первой строки
         text_item = soup.find('text', string=lambda text: f'_name_' in text)
         class_name = text_item.get('class', '').split(' ')[1]
@@ -139,14 +135,9 @@ def generate_svg_templates(json_data, base_svg_file):
         for item in items:
             max_text_lines = len(item['name'].split('\n'))
             shift_y += max_text_lines
+        print("lines in table", shift_y)
         shift_y = int((shift_y - 1) * table_line_height / 100)
         post_lines_keys = ["amount", "nds", "items", "total"]  # подстановки нижней части счета после таблицы
-
-        # Сохраняю шаблоны элементов
-        templates = {key: soup.find('text', string=lambda text: f'_{key}_' in text)
-                     for key in ['num', 'name', 'val', 'unit', 'price', 'sum']}
-
-        table_line_y = float(templates['num']['y'])
 
         # Проход и подстановка текста вокруг таблицы
         for key in base_keys:
@@ -168,21 +159,34 @@ def generate_svg_templates(json_data, base_svg_file):
                 for i, line in enumerate(text_lines):
                     new_elem = soup.new_tag('text', **{attr: text_elem[attr] for attr in text_elem.attrs})
                     new_elem.string = line
-                    new_elem['y'] = str(round(float(elem_y1 + i * line_height)))
+                    new_elem['y'] = str(round(elem_y1 + i * line_height))
                     new_elem['x'] = str(round(float(text_elem['x'])))
                     text_elem.insert_after(new_elem)
 
                     # записываю координаты и размер надписи
                     metrics = get_text_metrics(new_elem)[0]
+                    print('y', metrics[1], 'shifted y', metrics[1] + shift_y)
+
                     if key in post_lines_keys: metrics[1] += shift_y  # с учетом сдвига от всей таблицы
+                    print(key, 'metrics', metrics)
                     invoice['bbox_cx_cy_w_h'][key][i] = ', '.join(map(lambda x: str(x), metrics))
 
                 original_text_elem.decompose()
 
         # Проход и подстановка текста внутри таблицы
+
+        top_line = float(horizontal_lines[1]['y1'])
+        bottom_line = float(horizontal_lines[2]['y1'])
+        items_line_height = bottom_line - top_line
         border_y = top_line
 
         invoice['bbox_cx_cy_w_h']['itemsList'] = {}
+
+        # Сохраняю шаблоны элементов
+        templates = {key: soup.find('text', string=lambda text: f'_{key}_' in text)
+                     for key in ['num', 'name', 'val', 'unit', 'price', 'sum']}
+
+        table_line_y = float(templates['num']['y'])
 
         for n, item in enumerate(items):
             max_text_lines = len(item['name'].split('\n'))
@@ -198,15 +202,16 @@ def generate_svg_templates(json_data, base_svg_file):
                 for i, line in enumerate(text_lines):
                     new_elem = soup.new_tag('text', **{attr: template[attr] for attr in template.attrs})
                     new_elem.string = line
-                    new_elem['y'] = str(round(table_line_y + i*table_line_height))
-                    new_elem['x'] = str(round(template['x']))
+                    new_elem['y'] = str(round(table_line_y + i * table_line_height + 200))
+                    new_elem['x'] = str(round(float(template['x'])))
                     template.insert_after(new_elem)
                     metrics = get_text_metrics(new_elem)[0]
                     invoice['bbox_cx_cy_w_h']['itemsList'][n][key][i] = ', '.join(map(lambda x: str(x), metrics))
 
             # Обновление позиции курсора по Y после добавления строки
-            border_y += round(items_line_height * max_text_lines)
-            table_line_y += border_y
+            shift_line = round(items_line_height * max_text_lines)
+            border_y += shift_line
+            table_line_y += shift_line
 
             # Добавление разделительной линии
             new_breaking_line = soup.new_tag('line', **{attr: horizontal_lines[1][attr]
@@ -230,9 +235,9 @@ def generate_svg_templates(json_data, base_svg_file):
         # Сдвиг блока с ИТОГО вниз
         correct_svg_str = str(soup)
         correct_svg_str = correct_svg_str.replace('<g id="bottom" transform="matrix(1 0 0 1 0 0)">',
-                                                  f'<g transform="matrix(1 0 0 1 0 {shift_y})" id="bottom">')
+                                                  f'<g transform="matrix(1 0 0 1 0 {shift_y * 100})" id="bottom">')
 
-        invoice['magnet_stamp_y'] = str(int((magnet_stamp_y + shift_y / 100) * dim_scale))  # уровень по y для штампа
+        invoice['magnet_stamp_y'] = str(int((magnet_stamp_y + shift_y) * dim_scale))  # уровень по y для штампа
 
         # Сохраняю SVG
         file_name = f"invoice_{invoice['number']}.svg"
